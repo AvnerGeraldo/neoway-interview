804e5655dec5d70b742854cfe59b4dd68d0ac49b
Avner <avner.conceicao@hotmail.com>
2021-09-18 13:10:02 -0300

    fix: adicionar métodos do db no mesmo arquivo

--- app/database/models.py
+++ app/database/models.py
@@ -1,8 +1,26 @@
 from sqlalchemy import TIMESTAMP, func
+from flask import abort
 
 from . import db
 from .types import StatusEnum
-from helpers.db import saveModel, updateModel
+
+def saveModel(modelInstance):
+    try:
+        db.session.add(modelInstance)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        db.session.close()
+        abort(str(e), 500)
+
+def updateModel(modelInstance, updatedData: dict):
+    try:
+        modelInstance.update(updatedData)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        db.session.close()
+        abort(str(e), 500)
 
 class Customer(db.Model):
     __tablename__ = 'customer'
